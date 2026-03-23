03a74fa14e33469422d6a64800bf1f99e5fb039b
Witold Wojciechowski <witold.wojciechowski@appsvio.com>
2026-03-23 20:44:32 +0100

    z2

---
+++ lab2/script.js
@@ -0,0 +1,12 @@
+list = document.getElementById("taskList");
+
+function addTask(text,list){
+text = document.getElementById("taskInput").value;
+}
+function addList(){
+
+}
+function swapList(listSel){
+    list = listSel;
+    console.log(list);
+}
